ccfa963a60fc3839646fdd73688a6460f10933f5
Roman Bura <r2bcloud@gmail.com>
2021-10-05 02:26:19 -0400

    v1

---
+++ darkmode.js
@@ -0,0 +1,18 @@
+function $(el){
+  return document.querySelector(el);
+}
+
+let container = $('.container');
+let toggle = $('#toggle-theme');
+
+toggle.addEventListener('click', () => {
+  let list = container.classList;
+  
+  if(list.contains('default')){
+    list.replace('default','dark');
+    toggle.textContent = 'Light Mode';
+  }else{
+    list.replace('dark','default');
+    toggle.textContent = 'Dark Mode';
+  }
+})
